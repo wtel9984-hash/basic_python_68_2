fe7701202808a516c8b64beab724299fbb79bd86
Thalatchanan <wtel9984@gmail.com>
2025-11-22 14:10:04 +0700

    Operators

---
+++ python2.py
@@ -0,0 +1,50 @@
+"""
+#
+# Part: python operators
+# Arithmetic operators
+"""
+
+x = 4+5
+y = 10-2
+z = 3*7
+a = 8/2
+b = 10%3
+
+"""
+#
+# Part: python operators
+# Arithmetic operators
+#
+"""
+x = 5
+x = x +3
+x += 3
+
+"""
+#
+# Part: python operators
+# Arithmetic operators
+#
+"""
+x = 5
+y = 10
+print(x == y) #False
+print(x != y) #True
+print(x > y)  #False
+print(x < y)  #True
+print(x >= y) #False
+print(x <= y) #True
+
+"""
+#
+# Part: python operators
+# logical operators
+#
+"""
+x = 5
+y = 10 
+print((x < 10) and ( y > 5))  #True
+print((x < 10) or (y < 5))    #True
+print(not(x < 10))            #False
+print((x < 10) & (y > 5))     #True
+print((x < 10) | (y < 5))     #True
