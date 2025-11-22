8a37d722bdc4255d3e69784492185911b6ecfd34
Thalatchanan <wtel9984@gmail.com>
2025-11-22 13:29:58 +0700

    Variabal

---
+++ python1.py
@@ -0,0 +1,25 @@
+"""
+#
+#past: python com
+#
+"""
+
+ 
+"""
+v = s / t
+v = ความเร็ว (m/s)
+s = ระยะทาง (m)
+t = เวลา (s)
+"""
+print("Hello, World!")
+
+x = 5
+y = "5"
+print(x,y)
+
+
+x = str(5)
+y = int(10)
+z = float(15)
+print(type(x) , type(y) , type(z))
+
